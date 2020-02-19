0a2244b3f41ec8911d38c9153b433b733b881d57
1577351xyh <564146958@qq.com>
2020-02-20 00:20:01 +0800

    funtion组件状态更改 Hook Api

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,30 +1,48 @@
 import React, { Component } from 'react'
 
-export default class Clock extends React.Component {
-  // eslint-disable-next-line no-useless-constructor
-  constructor(props) {
-    //初始化
-    super(props)
-    //使用state属性维护状态,构造函数中初始化
-    this.state = { date: new Date() }
-  }
+//Hook Api
+import { useState, useEffect } from "react";
 
-  componentDidMount() {
-    //api的调用通常在这个构造函数中使用
-    this.timerId = setInterval(() => {
-      this.setState({
-        date: new Date()
-      });
-    }, 1000)
-  }
+// export default class Clock extends React.Component {
+//   // eslint-disable-next-line no-useless-constructor
+//   constructor(props) {
+//     //初始化
+//     super(props)
+//     //使用state属性维护状态,构造函数中初始化
+//     this.state = { date: new Date() }
+//   }
 
-  componentWillUnmount() {
-    //组件卸载的钩子函数
-    clearInterval(this.timerId)
-  }
+//   componentDidMount() {
+//     //api的调用通常在这个构造函数中使用
+//     this.timerId = setInterval(() => {
+//       this.setState({
+//         date: new Date()
+//       });
+//     }, 1000)
+//   }
 
-  render() {
-  return <div>{this.state.date.toLocaleTimeString()}</div>
-  }
+//   componentWillUnmount() {
+//     //组件卸载的钩子函数
+//     clearInterval(this.timerId)
+//   }
+//   render() {
+//   return <div>{this.state.date.toLocaleTimeString()}</div>
+//   }
+// }
 
+function Clock() {
+  const [date, setDate] = useState(new Date())
+  // useEffect编写异步操作代码(副作用代码)
+  //参数2传递空数组,使我们的函数就执行一次
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setDate(new Date())
+    })
+    //返回清理函数,组件销毁的时候会被清理
+    return () => { clearInterval(timerId) }
+  }, [setDate])
+
+  return <div>{date.toLocaleTimeString()}</div>
 }
+
+export default Clock
